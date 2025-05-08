0162c3e24021f2ed2ecdffee0af5438cd5a4ddfe
Thom066 <thomasnzcp78@gmail.com>
2025-05-07 19:11:07 -0500

    Add files via upload

---
+++ agendaContactos.py
@@ -0,0 +1,79 @@
+agendaContactos={}
+
+def crearContacto():
+    
+    print("Crear nuevo contacto")
+
+    nombre = input("Ingresa el nombre: ")
+
+    telefono = input("Ingresa el numero: ")
+    
+    correo = input("Ingresa el correo: ")
+    
+    print("---"*10)
+
+    agendaContactos[nombre]={
+    'telefono': telefono,
+    'correo' : correo
+    }
+    print("Creaste un contacto")
+
+    print("---"*10)
+
+def verContactos():
+
+    print("---"*10)
+
+    print("Viendo contactos")
+    print(agendaContactos)
+
+    print("---"*10)
+
+def buscarContactos():
+    buscar = input("Digita el nombre: ")
+    print("---" * 10)
+
+    contacto = agendaContactos.get(buscar)
+
+    if contacto:
+        print("Contacto encontrado")
+        print("---" * 10)
+        print("Nombre:",buscar)
+        print("Telefono: ", contacto["telefono"])
+        print("Correo: ", contacto["correo"])
+    else :
+        print("El contacto no existe")
+
+    print("---"*10)
+
+def actualizarContactos():
+    print("---"*10)
+    print("Entraste a actualizar contactos")
+    print("---"*10)
+    print(agendaContactos)
+    nom1 = input("Que contacto deseas actualizar: ")
+    
+
+
+
+
+
+    
+    
+
+    
+
+
+while True:
+    print("1) Crear contacto \n 2) Ver contactos \n 3) Buscar contactos \n 4) Actualizar contactos \n 5) Eliminar contactos \n 6) salir")
+
+    opcion = input ("Ingrese una opcion--->")
+
+    if opcion == "1":
+        crearContacto()
+    elif opcion == "2":
+        verContactos()
+    elif opcion == "3":
+        buscarContactos()
+    elif opcion == "4":
+        actualizarContactos()
